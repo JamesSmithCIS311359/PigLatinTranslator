21983b083133ea0d4541c0963f0b1c42c226a2ac
TheJPS <j.smith@pack.csupueblo.edu>
2023-11-07 22:01:22 -0700

    Completed.

--- PigLatinTranslator/Form1.cs
+++ PigLatinTranslator/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -37,7 +38,7 @@ namespace PigLatinTranslator
                 string piglatin = "";
                 foreach (string word in words)
                 {
-                    piglatin += TranslateWord(word) + " ";
+                    piglatin += TranslateWordWithCaps(word) + " ";
                 }
                 txtPigLatin.Text = piglatin;
             }
@@ -47,6 +48,29 @@ namespace PigLatinTranslator
                 txtEnglish.Focus();
             }
         }
+
+        private string TranslateWordWithCaps(string word)
+        {
+            string punct = "";
+            if (word.EndsWith(".") || word.EndsWith(",") || word.EndsWith(";")
+             || word.EndsWith(":") || word.EndsWith("!") || word.EndsWith("?"))
+            {
+                punct = word.Substring(word.Length - 1);
+                word = word.Remove(word.Length - 1, 1);
+            }
+
+            if (IsInitialCap(word))
+                word = ToInitialCap(TranslateWord(word));
+            if (IsUpper(word))
+                word = TranslateWord(word).ToUpper();
+            if (IsLower(word))
+                word = TranslateWord(word).ToLower();
+            
+            word += punct;
+
+            return word;
+        }
+
         private string TranslateWord(string word)
         {
             char c = word[0];
@@ -63,7 +87,7 @@ namespace PigLatinTranslator
                     word += c.ToString();
                     c = word[0];
                 }
-                while (c != 'a' &&  c != 'e' && c != 'i' && c != 'o' && c != 'u' 
+                while (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u'
                     && c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U' && c != 'y' && c != 'Y')
                 {
                     word = word.Remove(0, 1);
@@ -75,5 +99,59 @@ namespace PigLatinTranslator
 
             return word;
         }
+
+
+        private bool IsUpper(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (IsUpper(word[i]) == false)
+                    return false;
+            }
+                return true;               
+            
+        }
+        private bool IsLower(string word)
+        {
+            for (int i = 0; i < word.Length;i++)
+                if (IsLower(word[i])== false)
+                return false;
+            return true;
+        }
+
+        private bool IsInitialCap(string word)
+        {
+            char firstLetter = word[0];
+            string otherLetters = word.Remove(0, 1);
+            if (IsUpper(firstLetter) && IsLower(otherLetters))
+                return true;
+            return false;
+        }
+
+        private bool IsUpper(char c)
+        {
+            if (c >= 65 && c <= 90 || c.ToString() == "'")
+              return true;
+            else
+              return false;
+            }
+            
+        private bool IsLower(char c)
+        {
+            if (c >= 97 && c <= 122 || c.ToString() == "'")
+                return true;
+            else 
+                return false;
+
+        }
+
+        private string ToInitialCap(string word)
+        {
+            string firstLetter = word.Substring(0, 1).ToUpper();
+            string otherLetters = word.Substring(1).ToLower();
+            return firstLetter + otherLetters;
+        }
     }
 }
+
+
